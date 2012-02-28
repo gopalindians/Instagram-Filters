3b72f638a6acce69e37cfa6e656d1d3f31d1cacf
Di Wu <diwufet@gmail.com>
2012-02-28 10:05:54 +0800

    GPUImage added

---
+++ InstaFilters/GPUImage/GPUImage.h
@@ -0,0 +1,14 @@
+#import "GLProgram.h"
+
+// Base classes
+#import "GPUImageOpenGLESContext.h"
+#import "GPUImageOutput.h"
+#import "GPUImageView.h"
+#import "GPUImageVideoCamera.h"
+#import "GPUImageMovie.h"
+#import "GPUImagePicture.h"
+#import "GPUImageRawData.h"
+
+// Filters
+#import "GPUImageFilter.h"
+#import "GPUImageRotationFilter.h"
