fe98c35a334b0062cd903cf929ce6335cc652852
Di Wu <diwufet@gmail.com>
2012-02-28 12:24:28 +0800

    Toggle Filters Table View button added.

--- InstaFilters/FiltersViewController/IFFiltersViewController.m
+++ InstaFilters/FiltersViewController/IFFiltersViewController.m
@@ -13,7 +13,11 @@
 @property (nonatomic, strong) UIButton *backButton;
 @property (nonatomic, strong) UIImageView *backgroundImageView;
 @property (nonatomic, strong) UIImageView *cameraToolBarImageView;
+@property (nonatomic, strong) UIImageView *cameraCaptureBarImageView;
+@property (nonatomic, strong) UIButton *toggleFiltersButton;
+@property (nonatomic, unsafe_unretained) BOOL isFiltersTableViewVisible;
 - (void)backButtonPressed:(id)sender;
+- (void)toggleFiltersButtonPressed:(id)sender;
 @end
 
 @implementation IFFiltersViewController
@@ -21,6 +25,9 @@
 @synthesize backButton;
 @synthesize backgroundImageView;
 @synthesize cameraToolBarImageView;
+@synthesize cameraCaptureBarImageView;
+@synthesize toggleFiltersButton;
+@synthesize isFiltersTableViewVisible;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -39,6 +46,7 @@
     
     self.backgroundImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     self.backgroundImageView.image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraBackground" ofType:@"png"]];
+    
     self.cameraToolBarImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
     self.cameraToolBarImageView.image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraToolbar" ofType:@"png"]];
     
@@ -47,12 +55,24 @@
     [self.backButton setImage:[UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraIconCancel" ofType:@"png"]] forState:UIControlStateNormal];
     self.backButton.adjustsImageWhenHighlighted = NO;
     self.backButton.showsTouchWhenHighlighted = YES;
-
     [self.backButton addTarget:self action:@selector(backButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.cameraCaptureBarImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 426, 320, 54)];
+    self.cameraCaptureBarImageView.image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraCaptureBar" ofType:@"png"]];
+    
+    self.toggleFiltersButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.toggleFiltersButton.frame = CGRectMake(270, 430, 40, 40);
+    [self.toggleFiltersButton setImage:[UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraHideFilters" ofType:@"png"]] forState:UIControlStateNormal];
+    self.toggleFiltersButton.adjustsImageWhenHighlighted = NO;
+    self.toggleFiltersButton.showsTouchWhenHighlighted = YES;
+    [self.toggleFiltersButton addTarget:self action:@selector(toggleFiltersButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    self.isFiltersTableViewVisible = YES;
     
     [self.view addSubview:self.backgroundImageView];
     [self.view addSubview:self.cameraToolBarImageView];
     [self.view addSubview:self.backButton];
+    [self.view addSubview:self.cameraCaptureBarImageView];
+    [self.view addSubview:self.toggleFiltersButton];
 
 }
 
@@ -73,11 +93,24 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+#pragma mark - Button methods
+
 - (void)backButtonPressed:(id)sender {
     [self dismissViewControllerAnimated:YES completion:^() {
         // do nothing
     }];
 }
+
+- (void)toggleFiltersButtonPressed:(id)sender {
+    if (isFiltersTableViewVisible == YES) {
+        [self.toggleFiltersButton setImage:[UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraShowFilters" ofType:@"png"]] forState:UIControlStateNormal];
+        self.isFiltersTableViewVisible = NO;
+    } else {
+        [self.toggleFiltersButton setImage:[UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"glCameraHideFilters" ofType:@"png"]] forState:UIControlStateNormal];
+        self.isFiltersTableViewVisible = YES;
+    }
+}
+
 
 #pragma mark - View Will/Did Appear/Disappear
 - (void)viewWillAppear:(BOOL)animated {
